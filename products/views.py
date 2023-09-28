85af7437678f40c4d467cf6bcefb01ac0ab9d085
soukasamadi <>
2023-09-28 20:01:28 +0000

    Add product detail view and urlpattern

--- products/views.py
+++ products/views.py
@@ -69,3 +69,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show product details """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
