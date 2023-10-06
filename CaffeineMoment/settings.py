cff6404c8ce88f688cee67459a7bbd9e003c392d
soukasamadi <>
2023-10-06 11:31:31 +0000

    Add custom storage and Bucket config

--- CaffeineMoment/settings.py
+++ CaffeineMoment/settings.py
@@ -35,14 +35,15 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django_summernote',
     'django_countries',
-    'crispy_forms',
-    'crispy_bootstrap4',
     'home',
     'products',
     'bag',
     'profiles',
     'checkout',
     'banners',
+    # Other
+    'crispy_forms',
+    'crispy_bootstrap4',
     'storages',
 ]
 
@@ -182,6 +183,25 @@ AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
     'CacheControl': 'max-age=94608000',
 }
+
+
+if "USE_AWS" in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "caffeinemoment"
+    AWS_S3_REGION_NAME = "eu-west-1"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+    # Static and media files
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = "static"
+    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = "media"
+
+    # Override static and media URLs in production
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 
 # Stripe
