78e1294c1d8c850f21c1644ec2b0b1b92f5c6432
soukasamadi <>
2023-10-10 02:57:45 +0000

    Make contact inputs required

--- contact/models.py
+++ contact/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
     message = models.TextField()
 
     class Meta:
